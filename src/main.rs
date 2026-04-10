7172cba4dd98f7989501a838b2a6f26258e0f64f
polovni <luka.polovic11@gmail.com>
2026-04-10 19:19:29 +0200

    Error handling for input bytes

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,32 @@ use crossterm::terminal::{enable_raw_mode, disable_raw_mode};
 fn main() {
     enable_raw_mode().unwrap();
 
-    println!("Raw mode enabled! Press 'q' to quit.");
+    println!("Raw mode enabled! Press 'q' to quit.\r");
 
-    for b in io::stdin().bytes() {
-        let c = b.unwrap() as char;
-        
-        print!("{}", c);
+    for b_result in io::stdin().bytes() {
+        match b_result {
+            Ok(b) => {
+                let c = b as char;
 
-        io::stdout().flush().unwrap();
+                if c.is_control() {
+                    print!("Binary: {0:08b} ASCII: {0:#03}\r", b);
+                } else {
+                    print!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}\r", b, c);
+                }
 
-        if c == 'q' {
-            disable_raw_mode().unwrap();
-            println!("\nQuitting...");
-            break;
+                io::stdout().flush().unwrap();
+
+                if c == 'q' {
+                    break;
+                }
+            }
+
+            Err(err) => {
+                println!("\nAn error occurred: {}", err);
+                break;
+            }
         }
     }
+
+    disable_raw_mode().unwrap();
 }
