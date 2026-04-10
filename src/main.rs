2f55a7d36033b6591cb6f68e4b8637b116333bc3
polovni <luka.polovic11@gmail.com>
2026-04-10 18:44:23 +0200

    Entering RAW mode in the terminal

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::io::{self, Read, Write};
+use crossterm::terminal::{enable_raw_mode, disable_raw_mode};
+
 fn main() {
-    println!("Hello, world!");
+    enable_raw_mode().unwrap();
+
+    println!("Raw mode enabled! Press 'q' to quit.");
+
+    for b in io::stdin().bytes() {
+        let c = b.unwrap() as char;
+        
+        print!("{}", c);
+
+        io::stdout().flush().unwrap();
+
+        if c == 'q' {
+            disable_raw_mode().unwrap();
+            println!("\nQuitting...");
+            break;
+        }
+    }
 }
